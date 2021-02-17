7c19a59b9e411fc106424d4a6d7e3089faf34552
kienboeck <kienboec@technikum-wien.at>
2021-02-17 10:12:25 +0100

    first version creating a test.pdf beside the binary

---
+++ PDFGenerationInCSharp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using DinkToPdf;
+
+namespace PDFGenerationInCSharp
+{
+    class Program
+    {
+        public static readonly string HTMLPage = @"
+<html>
+<head><title>Test Page for generation</title></head>
+<body>
+<h1>Example</h1>
+<p>... if you can read this, you won the generation game</p>
+</body>
+</html>
+";
+
+
+
+        static void Main(string[] args)
+        {
+            // iText7Demo_not_working.CreatePDF();
+            // iText7Demo_not_working.ConvertHTML();
+
+            // https://github.com/rdvojmoc/DinkToPdf
+            // https://github.com/rdvojmoc/DinkToPdf/tree/master/v0.12.4/64%20bit
+            var converter = new BasicConverter(new PdfTools());
+            var doc = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                    ColorMode = ColorMode.Color,
+                    Orientation = Orientation.Landscape,
+                    PaperSize = PaperKind.A4Plus,
+                    Out = @"test.pdf",
+                },
+                Objects = {
+                    new ObjectSettings() {
+                        PagesCount = true,
+                        HtmlContent = HTMLPage,
+                        WebSettings = { DefaultEncoding = "utf-8" },
+                        HeaderSettings = { FontSize = 9, Right = "Page [page] of [toPage]", Line = true, Spacing = 2.812 },
+                        
+                    }
+                }
+            };
+            converter.Convert(doc);
+        }
+    }
+}
---
+++ PDFGenerationInCSharp/iText7Demo_not_working.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iText.Html2pdf;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+
+namespace PDFGenerationInCSharp
+{
+    public class iText7Demo_not_working
+    {
+        //https://kb.itextpdf.com/home/it7kb/examples/itext-7-jump-start-tutorial-chapter-1#c01e01_helloworld.cs
+        public static void CreatePDF()
+        {
+            // Must have write permissions to the path folder
+            PdfWriter writer = new PdfWriter("demo.pdf");
+            PdfDocument pdf = new PdfDocument(writer);
+            Document document = new Document(pdf);
+            Paragraph header = new Paragraph("HEADER")
+                .SetTextAlignment(TextAlignment.CENTER)
+                .SetFontSize(20);
+
+            document.Add(header);
+            document.Close();
+        }
+
+        public static void ConvertHTML()
+        {
+            File.WriteAllText("input.html", Program.HTMLPage);
+
+            // https://github.com/itext/i7n-pdfhtml
+            // https://itextpdf.com/en/products/itext-7/convert-html-css-to-pdf-pdfhtml
+            using Stream htmlSource = File.Open("input.html", FileMode.Open);
+            using FileStream pdfDest = File.Open("output.pdf", FileMode.OpenOrCreate);
+            ConverterProperties converterProperties = new ConverterProperties();
+            HtmlConverter.ConvertToPdf(htmlSource, pdfDest, converterProperties);
+        }
+    }
+}
